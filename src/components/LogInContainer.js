d76b23b9967af42427bf028f3058470e724476eb
IX <khyzhniak.i@outlook.com>
2023-03-30 13:15:43 +0000

    fix bg color

--- src/components/LogInContainer.js
+++ src/components/LogInContainer.js
@@ -7,5 +7,16 @@ export const LogInContainer = styled.div`
   justify-content: center;
   height: 100vh;
   width: 100vw;
-  background: #242731;
+  position: relative;
+  z-index: 1;
+  &::before {
+    content: "";
+    position: fixed;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #242731;
+  }
 `

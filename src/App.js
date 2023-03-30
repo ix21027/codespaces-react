7f45e948e956dc1dcd7c115fcec814d66a37dbef
IX <khyzhniak.i@outlook.com>
2023-03-30 16:34:15 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import { LogIn } from "./pages/login";
 import { SignUp } from "./pages/signup";
-import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
-// const LogIn = lazy(() => import('./pages/login'));
-
 
 function App() {
   return (
     <>
-      <Suspense fallback={<>Loading...</>}>
-      {/* <LogIn /> */}
-        <Routes>
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/dashboard" element={<>Loading...</>} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="*" element={<LogIn />} /> 
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/dashboard" element={<>Loading...</>} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<LogIn />} /> 
+      </Routes>
     </>
   )
 }
